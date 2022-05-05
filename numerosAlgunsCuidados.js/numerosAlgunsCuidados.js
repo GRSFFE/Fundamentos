3c79890326d0c4c7cf5871b22ae4b945b83d1c88
Germanno Resende <germannoresende@gmail.com>
2022-05-05 11:15:44 -0300

    Criei algumas pastas pro curso da Udemy de JS.

---
+++ numerosAlgunsCuidados.js/numerosAlgunsCuidados.js
@@ -0,0 +1,9 @@
+console.log(7 / 0) // Qualquer número dividido por 0 em JS, gera o Infinity.
+
+console.log("10" / 2) // Mesmo sendo string, quando tem um número o VSCode investiga oq tem dentro das Aspas.
+
+console.log("Show" * 2) // Em outras linguagens o "show" sairia duas vezes, mas não no JS, que só multiplica se for número.
+
+console.log(0.1 + 0.7) // Gera uma imprecisão por causa do ponto flutuante. 
+
+console.log((10.345).toFixed(2))
